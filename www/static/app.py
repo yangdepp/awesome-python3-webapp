470ea16d465923bcb4640b6714082188cfd08dcf
yangdepp <965093975@qq.com>
2018-04-18 00:04:01 +0800

    dev async

--- www/static/app.py
+++ www/static/app.py
@@ -7,7 +7,7 @@ __author__ = "yangdepp"
 asnyc web application
 '''
 #logging模块用于输出运行日志,logging.basicConfig()函数实现打印日志的基础配置
-import logging; logging.basicConfig(level=logging.INFO)  
+import logging; logging.basicConfig(level=logging.INFO)
 import asyncio, os, json, time
 from datetime import datetime
 from aiohttp import web
@@ -22,11 +22,10 @@ def index(request):
     return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html',charset='utf-8')
 
 
-@asyncio.coroutine
-def init(loop):
+async def init(loop):
     app = web.Application(loop=loop)         #创建web服务器实例app
     app.router.add_route('GET', '/', index)       #将url注册进router
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)      #yield from返回一个创建好的，绑定ip，端口和HTTP协议监听协程
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)      #yield from返回一个创建好的，绑定ip，端口和HTTP协议监听协程
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 

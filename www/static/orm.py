88ff66144e07d70eda33225d8f4692fd57ba88e1
yangdepp <965093975@qq.com>
2018-08-08 23:18:12 +0800

    metaclass

--- www/static/orm.py
+++ www/static/orm.py
@@ -1,1 +1,9 @@
 # 一个orm
+class ListMetaClass(type):
+    def __new__(cls, name, bases, attrs):
+        attrs['add'] = lambda self, value: self.append(value)
+        return type.__new__(cls, name, bases, attrs)
+
+
+class MyList(list, metaclass=ListMetaClass):
+    pass

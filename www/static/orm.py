194b9ed8c0aa7c51415cc90a0f365ea8d7affdfa
yangdepp <965093975@qq.com>
2018-08-19 22:58:23 +0800

    awesome orm

--- www/static/orm.py
+++ www/static/orm.py
@@ -9,12 +9,115 @@ def log(sql, args=()):
 
 # 创建一个全局的连接池，每个HTTP请求都可以从连接池中直接获取数据库连接
 # 使用连接池的好处是：不用频繁的打开和关闭数据库连接，能复用就复用
-
-
 async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
     __pool = await aiomysql.create_pool(
-        host=kw.get('host', 'localhost')
-
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
     )
+
+
+# 定义一个select函数，执行select语句
+async def select(sql, args, size=None):
+    log(sql, args)
+    global __pool
+    # __pool.get()是从连接池中取出一个连接对象
+    async with __pool.get() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(sql.replace('?', '%s'), args or ())
+            # 如果传入size参数，就通过fetchmany()获取最多指定数量的记录，否则获取所有记录
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+
+# Insert, Update, Delete可以定义一个通用的excute()函数
+async def execute(sql, args, autocommit=True):
+    log(sql)
+    async with __pool.get() as conn:
+        if not autocommit:
+            await conn.begin()
+        try:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(sql.replace('?', '%s'), args)
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
+        except BaseException as e:
+            if not autocommit:
+                await conn.rollback()
+            raise
+        return affected
+
+
+# 这个函数主要是把查询字段计数 替换成sql识别的?
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+    return ', '.join(L)
+
+
+class Field(object):
+
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s:%s>'.format(self.__class__.__name__, self.column_type, self.name)
+
+
+class StringField(Field):
+    pass
+
+
+class BooleanField(Field):
+    pass
+
+
+class IntegerField(Field):
+    pass
+
+
+class FloatField(Field):
+    pass
+
+
+class TextField(Field):
+    pass
+
+
+class ModelMetaclass:
+    pass
+
+
+class Model(dict, metaclass=ModelMetaclass):
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'model' object has no attribute '%s'".format(key))
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)

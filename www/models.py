7bfe61c05f36a1256db9dc025817a27283223732
yangdepp <965093975@qq.com>
2018-08-22 13:15:01 +0800

    调整目录

---
+++ www/models.py
@@ -0,0 +1,49 @@
+# create by 'yang' in 2018/8/20 
+__author__ = 'yang'
+
+import time, uuid
+from orm import Model, StringField, BooleanField, FloatField, TextField
+
+
+def next_id():
+    return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
+
+
+class User(Model):
+    'User类映射Mysql数据库中的User表'
+    __table__ = 'users'  # 表名
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')  # 主键
+    email = StringField(ddl='varchar(50)')  # 作为登陆账号
+    passwd = StringField(ddl='varchar(50)')  # 列密码
+    admin = BooleanField()
+    name = StringField(ddl='varchar(50)')
+    image = StringField(ddl='varchar(500)')
+    created_at = FloatField(default=time.time)
+
+
+class Blog(Model):
+    'Blog类映射Mysql数据库中的blog表'
+    __table__ = 'blogs'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    name = StringField(ddl='varchar(50)')
+    summary = StringField(ddl='varchar(200)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
+
+
+class Comment(Model):
+    'Comment评论类映射MySQL数据库中的评论表'
+    __table__ = 'comments'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    blog_id = StringField(ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
